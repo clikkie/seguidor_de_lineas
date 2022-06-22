6c117fd04385500557338e8d5028b5b58359a19b
clikkie <lilliangam18@alumno.hayas.edu.mx>
2022-06-21 22:34:36 -0500

    filtro y SD

--- Prueba 1/main.cpp
+++ Prueba 1/main.cpp
@@ -21,8 +21,16 @@ int lec2 = 0;
 int lec3 = 0;
 int lec4 = 0;
 int lec5 = 0;
+
 int i = 0;
 int n = 10;
+
+int SD0 = 0;
+int SD1 = 0;
+int SD2 = 0;
+int SD3 = 0;
+int SD4 = 0;
+int SD5 = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -49,17 +57,83 @@ for(i = 0; i < n; i++)
   lec4 = lec4/n;
   lec5 = lec5/n;
 
+if(lec0 > 900)
+   {
+      SD0 = 1024;
+    }
+  else
+   {
+      SD0 = 0;
+  }
+
+if(lec1 > 900)
+   {
+      SD1 = 1024;
+    }
+  else
+   {
+      SD1 = 0;
+  }
+
+if(lec2 > 900)
+   {
+      SD2 = 1024;
+    }
+  else
+   {
+      SD2 = 0;
+  }
+
+if(lec3 > 900)
+   {
+      SD3 = 1024;
+    }
+  else
+   {
+      SD3 = 0;
+  }
+
+if(lec4 > 900)
+   {
+      SD4 = 1024;
+    }
+  else
+   {
+      SD4 = 0;
+  }
+
+if(lec4 > 900)
+   {
+      SD5 = 1024;
+    }
+  else
+   {
+      SD5 = 0;
+  }
+
     Serial.print(lec0);
   Serial.print(",");
+    Serial.print(SD0);
+  Serial.print(".");
     Serial.print(lec1);
+  Serial.print(",");
+    Serial.print(SD1);
   Serial.print(",");
     Serial.print(lec2);
   Serial.print(",");
+    Serial.print(SD2);
+  Serial.print(",");
     Serial.print(lec3);
+  Serial.print(",");
+    Serial.print(SD3);
   Serial.print(",");
     Serial.print(lec4);
   Serial.print(",");
-    Serial.println(lec5);
+    Serial.print(SD4);
+  Serial.print(",");
+    Serial.print(lec5);
+  Serial.print(",");
+    Serial.println(SD5);
   delay(1);
 
 }
